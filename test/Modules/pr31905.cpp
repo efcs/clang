dd4a10e9914412197387fed070603cd860284be0
Eric Fiselier <eric@efcs.ca>
2018-09-30 20:44:27 -0400

    fix test

--- test/Modules/pr31905.cpp
+++ test/Modules/pr31905.cpp
@@ -1,8 +1,9 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
 // RUN: cp -R %S/Inputs/PR31905/my-project-root/ %t/other-include
-// RUN: %clang_cc1 -std=c++11 -isystem %S/Inputs/PR31905/system-dir \
+// RUN: %clang_cc1 -std=c++11 \
 // RUN:    -I%S/Inputs/PR31905/my-project-root -I%t/other-include \
+// RUN:    -I%S/Inputs/PR31905/system-dir \
 // RUN:    -fmodules -fmodules-local-submodule-visibility \
 // RUN:    -fimplicit-module-maps -fmodules-cache-path=%t  -verify %s
 
